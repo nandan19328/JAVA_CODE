45c6b0fe468e412fd1b9f375645c67bca4ef300e
nandanverma <nandanverma103536@gmail.com>
2024-05-04 17:12:46 +0530

    ArrayIndex Program

---
+++ arrayindex.java
@@ -0,0 +1,16 @@
+public class arrayindex {
+    
+        public static void main(String[] args)
+        {
+            int[] arr = new int[4];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+    
+           
+            System.out.println(arr[3]);
+        }
+    }
+    
+
