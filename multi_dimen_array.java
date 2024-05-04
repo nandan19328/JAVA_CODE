73341abeb22b7a3e39da53d3750ef1c71fcea703
nandanverma <nandanverma103536@gmail.com>
2024-05-04 17:57:04 +0530

    mutlidimensional array

---
+++ multi_dimen_array.java
@@ -0,0 +1,19 @@
+public class multi_dimen_array {
+    
+        public static void main(String[] args)
+        {
+           
+            int[][] arr = new int[4][3];
+           
+    
+           
+            System.out.println("Number of Rows:"+
+                               arr.length);
+          
+              
+            System.out.println("Number of Columns:"+
+                               arr[0].length);
+        }
+    }
+    
+
